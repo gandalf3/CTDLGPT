41c758f387e11dcda6f6c98648f7f9dc2f21cc67
zal <rj.atticus@gmail.com>
2019-09-04 04:16:05 -0700

    no more enumersated_sorting_view

--- inbox.js
+++ inbox.js
@@ -1,9 +1,17 @@
 'use_strict';
 
+
+
+let sorting_type = document.location.href.split('#')[1]
+let title = document.getElementById('title')
+
+
 // Create a single ThougtCanvas object and give it the thought canvas container
 // element already in the html file
 let canv = document.getElementById('thought-canvas-container');
-var TC = new ThoughtCanvas(canv, null);
+var TC = new ThoughtCanvas(canv, title);
+
+
 
 // Set a key release event listener on the input element to listen for releases
 // of "enter" and add its content to the ThoughtCanvas when it finds one
@@ -16,6 +24,8 @@ inp.addEventListener('keyup', event => {
 
    inp.value = '';
 });
+
+title.textContent = sorting_type
 
 // focus the input by default
 inp.focus();
